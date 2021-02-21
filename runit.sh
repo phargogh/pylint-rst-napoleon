1c6d9be72c0bb6bdc21599f4e0b1c253d107eab6
James Douglass <jamesdouglassusa@gmail.com>
2021-02-21 13:36:58 -0800

    cloning taskgraph if needed.

--- runit.sh
+++ runit.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ ! -d taskgraph ]]
+then
+    git clone https://github.com/natcap/taskgraph.git
+fi
+
 PYTHONPATH=. python -m pylint \
     --errors-only \
     --load-plugins plugin \
